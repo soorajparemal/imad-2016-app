3aac515f2b524af1a0e80f2fe8a29fa8764e16a0
Sooraj Paremal <srjps202@gmail.com>
2016-09-28 11:35:54 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,17 @@
-console.log('Loaded!');
+var button=document.getElementById('counter');
+
+button.onClick=function() {
+    var request=new XMLHttpRequest();
+    
+    request.onreadystatechange = function(){
+        if(request.readyState===XMLHttpRequest.DONE){
+            if(request.status===200){
+                var counter = request.responseText;
+                var span=document.getElementById('count');
+                span.innerHTML=counter.toString();
+            }
+        }
+    }
+    
+    request.open('GET','http://soorajparemal.imad.hasura-app.io/counter')
+}
