14bd39c3626530be53c2bfa9ef90167dbe8fd655
Sooraj Paremal <srjps202@gmail.com>
2016-10-11 11:07:49 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -26,6 +26,7 @@ button.onclick = function() {
     
 };
 var img=document.getElementById('madi');
+var marginLeft=0;
 function moveRight(){
     marginLeft = marginLeft + 10;
     img.style.marginLeft = marginLeft + 'px';
